41ae27fdc45339941c76655aae8f89e436a6fa30
Elie Vogel <vogel.elie@outlook.com>
2015-08-03 08:47:48 +0100

    Better palindrome matching for complex sentences with non-alphanumeric characters.

--- Bonfire-Palindrome/palindrome.js
+++ Bonfire-Palindrome/palindrome.js
@@ -1,21 +1,29 @@
-function palindrome(str) {
-  asArray = str.split();
-  // midpoint different if even or odd
-  length = str.length;
+function arrayMidpoint(length){
   if(length % 2 == 1)
     midpoint = (length-1) / 2;
   else
-    midpoint = length / 2 - 1;
-  
-  console.log("Midpoint s[" + midpoint + "] : " + str.charAt(midpoint));
-  palindrome = true;
-  /**while(palindrome && index ){
-    if(asArray[i] != asArray[])
-      palindrome = False;
-    
-  }
-  return palindrome;*/
-  return;
+    midpoint = length / 2 - 1; 
+  return midpoint;
 }
 
-palindrome("helpo");
+function palindrome(str) {
+  str = str.replace(/[^A-z]/g, ""); // get rid of non alphabetic characters
+  str = str.toLowerCase();
+  asArray = str.split("");
+  console.log("String: " + str);
+  // midpoint different if even or odd
+  midpoint = arrayMidpoint(str.length);
+
+  console.log("Midpoint s[" + midpoint + "] : " + str.charAt(midpoint));
+  ispalindrome = true;
+  var sindex = 0, eindex=str.length-1;
+  while(palindrome && sindex <= midpoint ){
+    console.log("comparing " + asArray[sindex] + " and " + asArray[eindex]);
+    if(asArray[sindex] != asArray[eindex])
+      ispalindrome = false;
+    sindex++;
+    eindex--;
+
+  }
+  return ispalindrome;
+}
