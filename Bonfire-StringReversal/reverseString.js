dcfb633788c5a133ffd8d2a5e2fa0f320a659a3e
Elie Vogel <vogel.elie@outlook.com>
2015-08-03 08:30:38 +0100

    Minor alteration for string reversal function

--- Bonfire-StringReversal/reverseString.js
+++ Bonfire-StringReversal/reverseString.js
@@ -1,8 +1,5 @@
 function reverseString(str) {
   var reversed = str.split();
-  j = 0;
-  for(var i = str.length - 1; i >= 0; i--){
-    reversed[j++] = str.charAt(i);
-  }
+  Array.reverse(reversed); 
   return reversed.join("");
 }
